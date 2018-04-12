6079ae5e40fabd17fbe98ef76a7b0c8d43ee9f13
hyde <songhaifeng@goldmantis.com>
2018-04-12 14:38:28 +0800

    learn by doing

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+ message: `It's easy to integrate React and Redux, isn't it? 是这样么？`,
+};
+
+function reducer(state = initialState) {
+ return state;
+}
+
+export default reducer;
